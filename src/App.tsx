c4c46ac3cd75aeaa88552c9559cb1e5d454a3b66
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-06-25 16:39:10 +0300

    dividing the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import WheatherBrief from "./components/wheater-brife/WheatherBrief";
+import WheatherHeader from "./components/wheater-header/wheather-header";
+import WheatherBox from "./components/wheather-box/WheatherBox";
 
 function App() {
 	return (
-		<div className="text-l font-bold underline bg-midnight text-tahiti">
-			hi
+		<div className="flex flex-col items-center h-screen pt-2 px-3">
+			<WheatherHeader />
+			<WheatherBrief />
+			<WheatherBox />
+			<div className="weather-row bg-orange-400">nowW</div>
 		</div>
 	);
 }
