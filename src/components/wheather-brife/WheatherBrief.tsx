93a1ace6e3e578bd14d72fcf3888aa59f424aab5
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-06-27 18:30:55 +0300

    making the weather brife dynamic

--- src/components/wheather-brife/WheatherBrief.tsx
+++ src/components/wheather-brife/WheatherBrief.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { WeatherResponse } from "../../helpers/interfaces/weatherRespone.interface";
 
-export default function WheatherBrief() {
+export default function WheatherBrief({
+	main: { temp },
+	weather,
+}: WeatherResponse) {
 	const currentDate = new Date();
 	const options: Intl.DateTimeFormatOptions = {
 		weekday: "long",
@@ -10,10 +14,12 @@ export default function WheatherBrief() {
 	const formattedDate = currentDate.toLocaleDateString(undefined, options);
 	return (
 		<div className=" w-full flex justify-evenly mb-8">
-			<div className="w-1/4 text-7xl h-20">20&deg;</div>
+			<div className="w-1/4 text-7xl h-20">{temp}&deg;</div>
 			<div className="w-0.5 bg-gray-500"></div>
 			<div className="flex flex-col justify-evenly pl-1">
-				<p className="capitalize font-bold text-lg">cloudy</p>
+				<p className="capitalize font-bold text-lg">
+					{weather[0].main}
+				</p>
 				<p className="text-gray-500">{formattedDate}</p>
 			</div>
 		</div>
