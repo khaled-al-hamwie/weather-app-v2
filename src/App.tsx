80276f8fab523ff69eaad1ad2e4f19a302b0ee2c
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-06-27 18:51:23 +0300

    passing the city name to the weather header

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
 	return (
 		<div className="flex flex-col items-center h-screen pt-2 px-3 bg-gray-100">
-			<WheatherHeader />
+			{weather ? <WheatherHeader {...weather} /> : <div>h</div>}
 			{weather ? <WheatherBrief {...weather} /> : <div>h</div>}
 			{weather ? <WheatherBox {...weather} /> : <div>h</div>}
 		</div>
--- src/components/wheather-header/WheatherHeader.tsx
+++ src/components/wheather-header/WheatherHeader.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../../assets/location-icon.png";
+import { WeatherResponse } from "../../helpers/interfaces/weatherRespone.interface";
 
-export default function WheatherHeader() {
+export default function WheatherHeader({ name }: WeatherResponse) {
 	return (
 		<div className="flex w-full justify-end mb-8">
-			<div className="font-bold capitalize text-xs pr-4">damascus</div>
+			<div className="font-bold capitalize text-xs pr-4">{name}</div>
 			<img src={logo} alt="location-icon" className="w-5 " />
 		</div>
 	);
