2a4c79aba6568399e1589275508ba7f03b8c80cd
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-06-27 18:31:19 +0300

    passing the weather info

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import WheatherBox from "./components/wheather-box/WheatherBox";
 import WheatherBrief from "./components/wheather-brife/WheatherBrief";
 import WheatherHeader from "./components/wheather-header/WheatherHeader";
+import getCoords from "./helpers/getCoords";
+import getWheather from "./helpers/getWheather";
+import { Coords } from "./helpers/interfaces/coords.interface";
+import { WeatherResponse } from "./helpers/interfaces/weatherRespone.interface";
 
+//render it
 function App() {
+	const [weather, setWheather] = useState<WeatherResponse>();
+	useEffect(() => {
+		getCoords()
+			.then((coords: Coords) => getWheather(coords.lat, coords.lon))
+			.then((data) => setWheather(data))
+			.catch(console.log);
+	}, []);
+
 	return (
 		<div className="flex flex-col items-center h-screen pt-2 px-3 bg-gray-100">
 			<WheatherHeader />
-			<WheatherBrief />
+			{weather ? <WheatherBrief {...weather} /> : <div>h</div>}
 			<WheatherBox />
-			<div className="weather-row bg-orange-400">nowW</div>
 		</div>
 	);
 }
